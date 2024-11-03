7df6361a2b6ec0704bfe92e3249ec736593fe8bd
ChichiMsdk <111235918+ChichiMsdk@users.noreply.github.com>
2024-11-03 21:53:14 +0100

    Pipeline creation

---
+++ src/renderer/vulkan/vulkan_pipeline.c
@@ -0,0 +1,110 @@
+#include "vulkan_pipeline.h"
+
+#include "renderer/rendererimpl.h"
+
+#include "core/filesystem.h"
+#include "core/darray.h"
+#include "core/logger.h"
+#include "core/assert.h"
+#include "core/yerror.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * TODO: Change error handling here
+ * Needs to take into account fopen failures
+ */
+VkResult
+vkLoadShaderModule(VkContext *pCtx, const char* pFilePath, VkDevice device, VkShaderModule* pOutShaderModule)
+{
+	FILE* pStream;
+	int errcode = OsFopen(&pStream, pFilePath, "rb");
+	if (errcode != 0)
+	{
+		char buffer[1024];
+		OsStrError(buffer, 1024, errno);
+		YERROR("%s", buffer);
+		return VK_ERROR_UNKNOWN;
+	}
+	fseek(pStream, 0, SEEK_END);
+	size_t fileSize = ftell(pStream);
+	YDEBUG("file size: %llu", fileSize);
+
+	uint32_t* pBuffer = DarrayReserve(uint32_t, fileSize / sizeof(uint32_t));
+	fseek(pStream, 0, SEEK_SET);
+
+	size_t elemCount = 1;
+	int count = OsFread(pBuffer, fileSize, fileSize, elemCount, pStream);
+	YDEBUG("count read %d", count);
+
+	errcode = OsFclose(pStream);
+	if (errcode != 0)
+	{
+		char buffer[1024];
+		OsStrError(buffer, 1024, errno);
+		YERROR("%s", buffer);
+		return VK_ERROR_UNKNOWN;
+	}
+
+	VkShaderModuleCreateInfo shaderModuleCreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+		.pNext = VK_NULL_HANDLE,
+		.codeSize = fileSize,
+		.pCode = pBuffer,
+	};
+	VkShaderModule shaderModule;
+	VK_CHECK(vkCreateShaderModule(device, &shaderModuleCreateInfo, pCtx->pAllocator, &shaderModule));
+	DarrayDestroy(pBuffer);
+	*pOutShaderModule = shaderModule;
+	return VK_SUCCESS;
+}
+
+VkResult
+vkPipelineInit(VkContext *pCtx, VkDevice device, const char* pFilePath)
+{
+	VkPipelineLayoutCreateInfo computePipelineLayoutCreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+		.pNext = VK_NULL_HANDLE,
+		.pSetLayouts = &pCtx->drawImageDescriptorSetLayout,
+		.setLayoutCount = 1,
+	};
+	VK_CHECK(vkCreatePipelineLayout(
+				device,
+				&computePipelineLayoutCreateInfo,
+				pCtx->pAllocator,
+				&pCtx->gradientComputePipelineLayout));
+
+	VkShaderModule computeDrawShader;
+	VK_CHECK(vkLoadShaderModule(pCtx, pFilePath, device, &computeDrawShader));
+
+	VkPipelineShaderStageCreateInfo pipelineShaderStageInfo = {
+		.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+		.pNext = VK_NULL_HANDLE,
+		.stage = VK_SHADER_STAGE_COMPUTE_BIT,
+		.module = computeDrawShader,
+		.pName = "main",
+	};
+	VkComputePipelineCreateInfo computePipelineCreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
+		.pNext = VK_NULL_HANDLE,
+		.layout = pCtx->gradientComputePipelineLayout,
+		.stage = pipelineShaderStageInfo,
+	};
+
+	uint32_t createInfoCount = 1;
+	VkPipelineCache pipelineCache = VK_NULL_HANDLE;
+
+	VK_CHECK(vkCreateComputePipelines(
+				device,
+				pipelineCache,
+				createInfoCount,
+				&computePipelineCreateInfo,
+				pCtx->pAllocator,
+				&pCtx->gradientComputePipeline));
+
+	vkDestroyShaderModule(device, computeDrawShader, pCtx->pAllocator);
+
+	return VK_SUCCESS;
+}
--- src/renderer/vulkan/vulkan_pipeline.h
+++ src/renderer/vulkan/vulkan_pipeline.h
@@ -2,84 +2,17 @@
 #define VULKAN_PIPELINE_H
 
 #include "renderer/vulkan/yvulkan.h"
-#include "core/filesystem.h"
-#include "core/darray.h"
-#include "core/logger.h"
-#include "core/assert.h"
+#include "core/yerror.h"
 
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
+YND VkResult vkLoadShaderModule(
+		VkContext*							pCtx,
+		const char*							pFilePath,
+		VkDevice							device,
+		VkShaderModule*						pOutShaderModule);
 
-
-/*
- * TODO: Change error handling here
- * Needs to take into account fopen failures
- */
-VkResult
-vkLoadShaderModule(VkContext *pCtx, const char* pFilePath, VkDevice device, VkShaderModule* pOutShaderModule)
-{
-	FILE* pStream;
-	int errcode = OsFopen(&pStream, pFilePath, "rb");
-	if (!errcode)
-	{
-		char buffer[1024];
-		OsStrError(buffer, 1024, errno);
-		YERROR("%s in %s:%d", buffer, __FILE__, __LINE__);
-		return VK_ERROR_UNKNOWN;
-	}
-	fseek(pStream, 0, SEEK_END);
-	size_t fileSize = ftell(pStream);
-	YDEBUG("file size: %llu", fileSize);
-
-	uint32_t* pBuffer = DarrayReserve(uint32_t, fileSize);
-	fseek(pStream, 0, SEEK_SET);
-	OsFread(pBuffer, pStream, 1, 1, fileSize);
-	OsFclose(pStream);
-
-	VkShaderModuleCreateInfo shaderModuleCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-		.pNext = VK_NULL_HANDLE,
-		.codeSize = fileSize,
-		.pCode = pBuffer,
-	};
-	VK_CHECK(vkCreateShaderModule(device, &shaderModuleCreateInfo, pCtx->pAllocator, pOutShaderModule));
-	DarrayDestroy(pBuffer);
-	return VK_SUCCESS;
-}
-
-VkResult
-vkPipelineInit(VkContext *pCtx, VkDevice device, const char* pFilePath)
-{
-	VkShaderModule computeDrawShader;
-	VK_CHECK(vkLoadShaderModule(pCtx, pFilePath, device, &computeDrawShader));
-
-	VkPipelineShaderStageCreateInfo pipelineShaderStageInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-		.pNext = VK_NULL_HANDLE,
-		.stage = VK_SHADER_STAGE_COMPUTE_BIT,
-		.module = computeDrawShader,
-		.pName = "main",
-	};
-	VkComputePipelineCreateInfo computePipelineCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
-		.pNext = VK_NULL_HANDLE,
-		// .layout = _gradientPipelineLayout,
-		.stage = pipelineShaderStageInfo,
-	};
-	uint32_t createInfoCount = 1;
-	VkPipelineCache pipelineCache = VK_NULL_HANDLE;
-	VkPipeline *pPipelines = VK_NULL_HANDLE;
-
-	VK_CHECK(vkCreateComputePipelines(
-				device,
-				pipelineCache,
-				createInfoCount,
-				&computePipelineCreateInfo,
-				pCtx->pAllocator,
-				pPipelines));
-
-	return VK_SUCCESS;
-}
+YND VkResult vkPipelineInit(
+		VkContext*							pCtx,
+		VkDevice							device,
+		const char*							pFilePath);
 
 #endif //VULKAN_PIPELINE_H
