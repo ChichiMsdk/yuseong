64e06afed06101ee7b569046de58555df89b2032
ChichiMsdk <111235918+ChichiMsdk@users.noreply.github.com>
2024-11-08 19:04:34 +0100

    macro for profiler

---
+++ src/profiler.h
@@ -0,0 +1,13 @@
+#ifndef PROFILER_H
+#define PROFILER_H
+
+#ifdef TRACY_ENABLE
+#include "TracyC.h"
+#else
+#define TracyCFrameMark
+#define TracyCZoneN(a, b, c)
+#define TracyCZoneEnd(a)
+
+#endif
+
+#endif // PROFILER_H
