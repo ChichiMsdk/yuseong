ff48d3e06eee6d58e984fd4af44b09fbdc52f234
ChichiMsdk <111235918+ChichiMsdk@users.noreply.github.com>
2025-01-29 15:32:50 +0100

    cleanup

--- src/renderer/vulkan/vulkan_command.c
+++ src/renderer/vulkan/vulkan_command.c
@@ -7,67 +7,71 @@
 #include <vulkan/vk_enum_string_helper.h>
 
 VkResult
-vkImmediateCommandCreate(YMB VkContext* pCtx, VulkanDevice device, VkAllocationCallbacks* pAllocator,
-		VkCommandPool* pOutCommandPool, VulkanCommandBuffer* pOutCommandBuffer)
+vkImmediateCommandCreate(
+	YMB VkContext*			    pCtx,
+	VulkanDevice			    device,
+	VkAllocationCallbacks*		    pAllocator,
+	VkCommandPool*			    pOutCommandPool,
+	VulkanCommandBuffer*		    pOutCommandBuffer)
 {
-	VkCommandPoolCreateInfo poolCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
-		.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
-	};
-	VK_CHECK(vkCreateCommandPool(device.handle, &poolCreateInfo, pAllocator, pOutCommandPool));
-	VkCommandBufferLevel cmdBufferLvl = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-	VK_CHECK(vkCommandBufferAllocate(device.handle, cmdBufferLvl, *pOutCommandPool, pOutCommandBuffer));
-	return VK_SUCCESS;
+    VkCommandPoolCreateInfo poolCreateInfo = {
+	.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+	.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
+    };
+    VK_CHECK(vkCreateCommandPool(device.handle, &poolCreateInfo, pAllocator, pOutCommandPool));
+    VkCommandBufferLevel cmdBufferLvl = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    VK_CHECK(vkCommandBufferAllocate(device.handle, cmdBufferLvl, *pOutCommandPool, pOutCommandBuffer));
+    return VK_SUCCESS;
 }
 
 YND VkResult
 vkCommandSubmitImmediate(pfnSubmit pfnSubmit, VulkanDevice device, VulkanImmediateSubmit submitCommand, void* pCtx)
 {
-	VkCommandBufferResetFlags	flags		= 0;
-	uint32_t					fenceCount	= 1;
+    VkCommandBufferResetFlags	flags	    = 0;
+    uint32_t			fenceCount  = 1;
 
-	VK_CHECK(vkResetFences(device.handle, fenceCount, &submitCommand.fence.handle));
-	VK_CHECK(vkResetCommandBuffer(submitCommand.commandBuffer.handle, flags));
+    VK_CHECK(vkResetFences(device.handle, fenceCount, &submitCommand.fence.handle));
+    VK_CHECK(vkResetCommandBuffer(submitCommand.commandBuffer.handle, flags));
 
-	VkCommandBufferBeginInfo commandBeginInfo	= {
-		.sType	= VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-		.flags	= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
-	};
+    VkCommandBufferBeginInfo commandBeginInfo	= {
+	.sType	= VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+	.flags	= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
+    };
 
-	VK_CHECK(vkBeginCommandBuffer(submitCommand.commandBuffer.handle, &commandBeginInfo));
-	pfnSubmit(pCtx, submitCommand.commandBuffer);
-	VK_CHECK(vkEndCommandBuffer(submitCommand.commandBuffer.handle));
+    VK_CHECK(vkBeginCommandBuffer(submitCommand.commandBuffer.handle, &commandBeginInfo));
+    pfnSubmit(pCtx, submitCommand.commandBuffer);
+    VK_CHECK(vkEndCommandBuffer(submitCommand.commandBuffer.handle));
 
-	VkCommandBufferSubmitInfo commandSubmitInfo = {
-		.sType			= VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO,
-		.commandBuffer	= submitCommand.commandBuffer.handle,
-	};
-	VkSubmitInfo2 submitInfo2	= {
-		.sType					= VK_STRUCTURE_TYPE_SUBMIT_INFO_2,
-		.commandBufferInfoCount	= 1,
-		.pCommandBufferInfos	= &commandSubmitInfo,
-	};
-	uint32_t	submitInfoCount	= 1;
+    VkCommandBufferSubmitInfo commandSubmitInfo = {
+	.sType		= VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO,
+	.commandBuffer	= submitCommand.commandBuffer.handle,
+    };
+    VkSubmitInfo2 submitInfo2	= {
+	.sType			= VK_STRUCTURE_TYPE_SUBMIT_INFO_2,
+	.commandBufferInfoCount	= 1,
+	.pCommandBufferInfos	= &commandSubmitInfo,
+    };
+    uint32_t	submitInfoCount	= 1;
 
-	VK_CHECK(vkQueueSubmit2(device.graphicsQueue, submitInfoCount, &submitInfo2, submitCommand.fence.handle));
+    VK_CHECK(vkQueueSubmit2(device.graphicsQueue, submitInfoCount, &submitInfo2, submitCommand.fence.handle));
 
-	VkBool32	waitAll		= VK_TRUE;
-	VK_CHECK(vkWaitForFences(device.handle, fenceCount, &submitCommand.fence.handle, waitAll, 9999999999));
+    VkBool32	waitAll	= VK_TRUE;
+    VK_CHECK(vkWaitForFences(device.handle, fenceCount, &submitCommand.fence.handle, waitAll, 9999999999));
 
-	return VK_SUCCESS;
+    return VK_SUCCESS;
 }
 
 YND VkResult
 vkImmediateSubmitCommandCreate(VulkanDevice device, VulkanImmediateSubmit* pSubmit, VkAllocationCallbacks* pAllocator)
 {
-	VkCommandPoolCreateInfo poolCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
-		.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
-	};
-	VK_CHECK(vkCreateCommandPool(device.handle, &poolCreateInfo, pAllocator, &pSubmit->commandPool));
-	VkCommandBufferLevel cmdBufferLvl = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-	VK_CHECK(vkCommandBufferAllocate(device.handle, cmdBufferLvl, pSubmit->commandPool, &pSubmit->commandBuffer));
-	return VK_SUCCESS;
+    VkCommandPoolCreateInfo poolCreateInfo = {
+	.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+	.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
+    };
+    VK_CHECK(vkCreateCommandPool(device.handle, &poolCreateInfo, pAllocator, &pSubmit->commandPool));
+    VkCommandBufferLevel cmdBufferLvl = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    VK_CHECK(vkCommandBufferAllocate(device.handle, cmdBufferLvl, pSubmit->commandPool, &pSubmit->commandBuffer));
+    return VK_SUCCESS;
 }
 
 /**
@@ -77,101 +81,108 @@ vkImmediateSubmitCommandCreate(VulkanDevice device, VulkanImmediateSubmit* pSubm
 YND VkResult
 vkCommandPoolCreate(VulkanDevice device, VkAllocationCallbacks* pAllocator, VkCommandPool* pOutCommandPool)
 {
-	VkCommandPoolCreateInfo poolCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
-		.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
-	};
-	VK_CHECK(vkCreateCommandPool(device.handle, &poolCreateInfo, pAllocator, pOutCommandPool));
+    VkCommandPoolCreateInfo poolCreateInfo = {
+	.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+	.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
+    };
+    VK_CHECK(vkCreateCommandPool(device.handle, &poolCreateInfo, pAllocator, pOutCommandPool));
     YINFO("Graphics command pool created.");
-	return VK_SUCCESS;
+    return VK_SUCCESS;
 }
 
 YND VkResult 
 vkCommandBufferCreate(VkContext *pCtx)
 {
     if (!pCtx->pGfxCommands) 
-	{
-        pCtx->pGfxCommands = DarrayReserve(VulkanCommandBuffer, pCtx->swapchain.imageCount);
-        for (uint32_t i = 0; i < pCtx->swapchain.imageCount; ++i) {
-            memset(&pCtx->pGfxCommands[i], 0, sizeof(VulkanCommandBuffer));
-        }
+    {
+	pCtx->pGfxCommands = DarrayReserve(VulkanCommandBuffer, pCtx->swapchain.imageCount);
+	for (uint32_t i = 0; i < pCtx->swapchain.imageCount; ++i) {
+	    memset(&pCtx->pGfxCommands[i], 0, sizeof(VulkanCommandBuffer));
+	}
     }
     for (uint32_t i = 0; i < pCtx->swapchain.imageCount; ++i) 
+    {
+	if (pCtx->pGfxCommands[i].handle) 
 	{
-        if (pCtx->pGfxCommands[i].handle) 
-		{
-            /*
-             * vulkan_command_buffer_free(
-             *     &context,
-             *     context.device.graphics_command_pool,
-             *     &context.graphics_command_buffers[i]);
-             */
-        }
-        memset(&pCtx->pGfxCommands[i], 0, sizeof(VulkanCommandBuffer));
-        VK_CHECK(vkCommandBufferAllocate(pCtx->device.handle, VK_COMMAND_BUFFER_LEVEL_PRIMARY, 
-					pCtx->device.graphicsCommandPool, &pCtx->pGfxCommands[i]));
+	    /*
+	     * vulkan_command_buffer_free(
+	     *     &context,
+	     *     context.device.graphics_command_pool,
+	     *     &context.graphics_command_buffers[i]);
+	     */
+	}
+	memset(&pCtx->pGfxCommands[i], 0, sizeof(VulkanCommandBuffer));
+
+	VK_CHECK(vkCommandBufferAllocate(
+		    pCtx->device.handle,
+		    VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+		    pCtx->device.graphicsCommandPool,
+		    &pCtx->pGfxCommands[i]));
     }
     YINFO("Vulkan command buffers created.");
-	return VK_SUCCESS;
+    return VK_SUCCESS;
 }
 
 YND VkResult
-vkCommandBufferAllocate(VkDevice device, VkCommandBufferLevel cmdBufferLvl, VkCommandPool pool,
-		VulkanCommandBuffer *pOutCommandBuffers)
+vkCommandBufferAllocate(
+	VkDevice			    device,
+	VkCommandBufferLevel		    cmdBufferLvl,
+	VkCommandPool			    pool,
+	VulkanCommandBuffer*		    pOutCommandBuffers)
 {
-	memset(pOutCommandBuffers, 0, sizeof(VulkanCommandBuffer));
+    memset(pOutCommandBuffers, 0, sizeof(VulkanCommandBuffer));
 
-	VkCommandBufferAllocateInfo allocateInfo = {
-		.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
-		.level = cmdBufferLvl,
-		.pNext = VK_NULL_HANDLE,
-		.commandPool = pool,
-		.commandBufferCount = 1
-	};
-	VK_CHECK(vkAllocateCommandBuffers(device, &allocateInfo, &pOutCommandBuffers->handle));
-	pOutCommandBuffers->state = COMMAND_BUFFER_STATE_READY;
-	return VK_SUCCESS;
+    VkCommandBufferAllocateInfo allocateInfo = {
+	.sType		    = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+	.level		    = cmdBufferLvl,
+	.pNext		    = VK_NULL_HANDLE,
+	.commandPool	    = pool,
+	.commandBufferCount = 1
+    };
+    VK_CHECK(vkAllocateCommandBuffers(device, &allocateInfo, &pOutCommandBuffers->handle));
+    pOutCommandBuffers->state = COMMAND_BUFFER_STATE_READY;
+    return VK_SUCCESS;
 }
 
 YND VkResult
 vkCommandPoolDestroy(VkContext *pCtx, VkCommandPool *pCmdPools, uint32_t poolCount)
 {
-	VkDevice device = pCtx->device.handle;
-	VK_CHECK(vkDeviceWaitIdle(device));
-	for (uint32_t i = 0; i < poolCount; i++)
-		vkDestroyCommandPool(device, pCmdPools[i], pCtx->pAllocator);
-	return VK_SUCCESS;
+    VkDevice device = pCtx->device.handle;
+    VK_CHECK(vkDeviceWaitIdle(device));
+    for (uint32_t i = 0; i < poolCount; i++)
+	vkDestroyCommandPool(device, pCmdPools[i], pCtx->pAllocator);
+    return VK_SUCCESS;
 }
 
 YND VkResult
 vkCommandBufferFree(VkContext *pCtx, VulkanCommandBuffer *pCommandBuffers, VkCommandPool *pPool, uint32_t poolCount)
 {
-	VkDevice device = pCtx->device.handle;
-	VK_CHECK(vkDeviceWaitIdle(device));
-	for (uint32_t i = 0; i < poolCount; i++)
-		vkFreeCommandBuffers(device, *pPool, 1, &pCommandBuffers[i].handle);
+    VkDevice device = pCtx->device.handle;
+    VK_CHECK(vkDeviceWaitIdle(device));
+    for (uint32_t i = 0; i < poolCount; i++)
+	vkFreeCommandBuffers(device, *pPool, 1, &pCommandBuffers[i].handle);
     /*
-	 * NOTE: This here feels redundant 
-	 * if NULL then assume COMMAND_BUFFER_STATE_NOT_ALLOCATED
+     * NOTE: This here feels redundant 
+     * if NULL then assume COMMAND_BUFFER_STATE_NOT_ALLOCATED
      */
-	pCommandBuffers->handle = VK_NULL_HANDLE;
-	pCommandBuffers->state = COMMAND_BUFFER_STATE_NOT_ALLOCATED;
-	return VK_SUCCESS;
+    pCommandBuffers->handle = VK_NULL_HANDLE;
+    pCommandBuffers->state  = COMMAND_BUFFER_STATE_NOT_ALLOCATED;
+    return VK_SUCCESS;
 }
 
 void
 vkCommandBufferBegin(VulkanCommandBuffer* pCmd, b8 bSingleUse, b8 bRenderPassContinue, b8 bSimultaneousUse)
 {
     VkCommandBufferBeginInfo beginInfo = {
-		.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-		.flags = 0,
-	};
+	.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+	.flags = 0,
+    };
     if (bSingleUse) 
-        beginInfo.flags |= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+	beginInfo.flags |= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
     if (bRenderPassContinue) 
-        beginInfo.flags |= VK_COMMAND_BUFFER_USAGE_RENDER_PASS_CONTINUE_BIT;
+	beginInfo.flags |= VK_COMMAND_BUFFER_USAGE_RENDER_PASS_CONTINUE_BIT;
     if (bSimultaneousUse) 
-        beginInfo.flags |= VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
+	beginInfo.flags |= VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
 
     VK_ASSERT(vkBeginCommandBuffer(pCmd->handle, &beginInfo));
     pCmd->state = COMMAND_BUFFER_STATE_RECORDING;
@@ -180,15 +191,15 @@ vkCommandBufferBegin(VulkanCommandBuffer* pCmd, b8 bSingleUse, b8 bRenderPassCon
 YND VkResult 
 vkCommandBufferEnd(VulkanCommandBuffer* pCmd)
 {
-	VK_CHECK(vkEndCommandBuffer(pCmd->handle));
-	pCmd->state = COMMAND_BUFFER_STATE_RECORDING_ENDED;
-	return VK_SUCCESS;
+    VK_CHECK(vkEndCommandBuffer(pCmd->handle));
+    pCmd->state = COMMAND_BUFFER_STATE_RECORDING_ENDED;
+    return VK_SUCCESS;
 }
 
 YND VkResult 
 vkCommandBufferReset(VulkanCommandBuffer* pCmd, VkCommandBufferResetFlags flags)
 {
-	VK_CHECK(vkResetCommandBuffer(pCmd->handle, flags));
-	pCmd->state = COMMAND_BUFFER_STATE_READY;
-	return VK_SUCCESS;
+    VK_CHECK(vkResetCommandBuffer(pCmd->handle, flags));
+    pCmd->state = COMMAND_BUFFER_STATE_READY;
+    return VK_SUCCESS;
 }
